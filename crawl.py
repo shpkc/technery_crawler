df04c68142eb654465efe02d07753025078bb61d
shpkc <shpkc@naver.com>
2023-12-01 16:20:11 +0900

    feat : 리스트중 rss 존재하는곳만 수집

--- crawl.py
+++ crawl.py
@@ -19,6 +19,8 @@ def crawl():
     
     with open('db_tech_blog.yml') as f:
         community_list = yaml.full_load(f)
+        
+        print("수집중인 블로그 : " + str(len(community_list)) + "개")
 
         url: str = "https://nettbgrchfoegbakosnu.supabase.co"
         key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ldHRiZ3JjaGZvZWdiYWtvc251Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAzNjE1OTEsImV4cCI6MjAxNTkzNzU5MX0._dhLgVkgdYb1gXuHhfTUcGcG14s9Jw3akhPFbVmRiBo"
@@ -26,49 +28,55 @@ def crawl():
 
         for item in community_list:
             name = item["name"]
-            rss = item["rss"]
             
-            print(name + "탐색중")
-            
-            if hasattr(ssl, '_create_unverified_context'):
-                ssl._create_default_https_context = ssl._create_unverified_context
-                
-            feeds = feedparser.parse(rss)
-            
-            for feed in feeds.entries:
-                print("제목" + feed.title)
-                
-                updated = feed.updated
-                updated = updated.replace('GMT', '+0000')
-                updated = parse_date(updated)
-                updated = updated.replace(tzinfo=pytz.utc)
-                
-                isOneMonthAgo = is_one_month_ago(updated)
-                
-            
-                if isOneMonthAgo:
-                    data, count = supabase.table('posts').select('*').eq('title', feed.title).execute()
-                    isExist = len(data[1]) != 0
+            if "rss" in item:
                     
-                    time.sleep(5)
+                rss = item["rss"]
+                
+                
+                
+                print(name + "탐색중")
+                
+                if hasattr(ssl, '_create_unverified_context'):
+                    ssl._create_default_https_context = ssl._create_unverified_context
+                    
+                feeds = feedparser.parse(rss)
+                
+                for feed in feeds.entries:
+                    print("제목" + feed.title)
+                    
+                    updated = feed.updated
+                    updated = updated.replace('GMT', '+0000')
+                    updated = parse_date(updated)
+                    updated = updated.replace(tzinfo=pytz.utc)
+                    
+                    isOneMonthAgo = is_one_month_ago(updated)
                     
                 
-                    if not isExist:
-                        title = feed.title
-                        description = BeautifulSoup(feed.description, 'html.parser').get_text(separator=' ', strip=True)[0:80]
-                        link = feed.link
-                        thumbnail = og_image(feed.link)
-                        post_created_at = feed.updated
+                    if isOneMonthAgo:
+                        data, count = supabase.table('posts').select('*').eq('title', feed.title).execute()
+                        isExist = len(data[1]) != 0
                         
-                        print("새로 추가 : " + title)
+                        time.sleep(2)
+                        
                     
-                        data = supabase.table('posts').insert({
-                        "title":title,
-                        "description":description,
-                        "link":link,
-                        "thumbnail":thumbnail,
-                        "post_created_at":post_created_at,
-                        "author":name,
-                        }).execute()
+                        if not isExist:
+                            title = feed.title
+                            description = BeautifulSoup(feed.description, 'html.parser').get_text(separator=' ', strip=True)[0:80]
+                            link = feed.link
+                            thumbnail = og_image(feed.link)
+                            post_created_at = feed.updated
+                            textValue = BeautifulSoup(feed.description, 'html.parser').get_text(separator=' ', strip=True)
+                            
+                            print("새로 추가 : " + title)
+                        
+                            data = supabase.table("posts").insert({
+                            "title":title,
+                            "description":description,
+                            "link":link,
+                            "thumbnail":thumbnail,
+                            "post_created_at":post_created_at,
+                            "author":name,
+                            }).execute()
 
 
