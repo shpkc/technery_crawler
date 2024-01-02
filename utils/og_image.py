4776927fbc3991f9e78d098f27ac3274796c9e76
shpkc <shpkc@naver.com>
2024-01-02 20:30:37 +0900

    feat : og image 없을시 default url 추가

--- utils/og_image.py
+++ utils/og_image.py
@@ -13,4 +13,5 @@ def og_image(url):
         og_image_url = og_image_tag.get('content')
         return og_image_url
     else:
-        return None
+        default_img = "https://images.unsplash.com/photo-1547394765-185e1e68f34e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNvbXB1dGVyfGVufDB8fDB8fHww"
+        return default_img
